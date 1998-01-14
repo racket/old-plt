ed1e8eccbf574c0c62cb96fcbe1a27b8f7555070
Matthew Flatt <mflatt@racket-lang.org>
1998-01-14 15:26:11 +0000

    .

---
+++ src/mac/mred/MrEdSetup.h
@@ -0,0 +1,19 @@
+
+#define INCLUDE_WITHOUT_PATHS
+
+#ifdef __MWERKS__
+#if defined(__powerc)
+#include <MacHeadersPPC>
+#else
+#include <MacHeaders68K>
+#endif
+#endif
+
+#define WX_STANDARD_GRAPHICS 1
+
+#define WX_FORCE_APP_CREATION 1
+#define WXS_CANT_ASSIGN_STRUCTURES 1
+
+#define WXUNUSED(x)
+
+#define WXME_FOR_MRED 1
---
+++ src/mac/mzscheme/simpledrop.h
@@ -0,0 +1,12 @@
+
+/* Call to start: */
+extern void Drop_GetArgs(int *, char ***);
+
+/* You supply: */
+extern void Drop_Runtime(char **, int);
+extern void Drop_Quit(void);
+
+extern int scheme_mac_ready;
+extern int scheme_mac_argc;
+extern char **scheme_mac_argv;
+

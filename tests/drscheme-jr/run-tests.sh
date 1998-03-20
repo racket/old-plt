b9360db0ca3f0019b21c7e9fdd3090a4b399edc0
Paul Steckler <steck@stecksoft.com>
1998-03-20 23:46:15 +0000

    Adding drscheme-jr tests

---
+++ tests/drscheme-jr/run-tests.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+
+# test script for language levels
+# Author:  Paul Steckler
+# Revised: March 16 1998
+
+LEVELS="Beginner Intermediate Advanced R4RS+"
+
+for level in $LEVELS
+do 
+    echo -n "Running tests at $level level ... "
+    echo "$level level tests of DrScheme Jr" > $level.out
+    echo Date: `date` >> $level.out
+    drscheme-jr -x -l $level < the-tests.ss >> $level.out 2>&1
+    echo "done."
+done
+
+for level in $LEVELS
+do
+    echo Testing for differences at $level level ...
+    diff $level.out $level.std | awk '(NR > 5) { print }'
+    echo "Done."
+done
+

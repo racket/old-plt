638ce0b6ef15b2a3228f5c60a68b7b8dfadccab7
Matthew Flatt <mflatt@racket-lang.org>
1998-05-13 16:01:32 +0000

    .

--- src/mzscheme/dynsrc/init.cc
+++ src/mzscheme/dynsrc/init.cc
@@ -17,9 +17,18 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 extern "C" {
   int WINAPI dll_entry (HANDLE h, DWORD reason, void *ptr);
-  };
+};
 
-int WINAPI dll_entry (HANDLE , 		     DWORD reason,		     void *){
-  switch (reason)     {    case DLL_PROCESS_ATTACH:      break;
-    case DLL_PROCESS_DETACH:      break;    case DLL_THREAD_ATTACH:      break;
-    case DLL_THREAD_DETACH:      break;    }  return 1;}
+int WINAPI dll_entry (HANDLE, DWORD reason, void *){
+  switch (reason) {
+  case DLL_PROCESS_ATTACH:
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  }
+  return 1;
+}

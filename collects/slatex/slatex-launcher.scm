bb21d1f3b4ca4b41f0a673a742ac6a2dd92dea58
Robby Findler <robby@racket-lang.org>
1999-04-27 03:42:28 +0000

    ...

--- collects/slatex/slatex-launcher.scm
+++ collects/slatex/slatex-launcher.scm
@@ -1,9 +1,22 @@
 (require-library "slatex.ss" "slatex")
 (define slatex::*texinputs* #f)
 (define slatex::*texinputs-list* #f)
-(current-load 
- (lambda (fn) 
-   (let-values ([(base name dir?)
-		 (split-path fn)])
-     (when (string? base) (current-directory base))
-     (slatex::process-main-tex-file fn))))
+(let ([slatex-process-file
+       (lambda (fn)
+	 (let-values ([(base name dir?)
+		       (split-path fn)])
+	   (when (string? base) (current-directory base))
+	   (slatex::process-main-tex-file fn)))])
+  (case (system-type)
+    [(macos)
+
+     ;; set up drag and drop
+     (current-load slatex-process-file)
+
+     (for-each slatex-process-file (vector->list argv))]
+    [(windows unix)
+     (when (eq? (vector) argv)
+       (error 'slatex "expected a file on the command line~n"))
+     (slatex-process-file (vector-ref argv 0))
+     (system (format "latex ~a" (vector-ref argv 0)))
+     (exit)]))

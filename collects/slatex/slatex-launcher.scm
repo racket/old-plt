595a3d49c82d037364fe5ec7b555ccc365190b66
Robby Findler <robby@racket-lang.org>
1999-05-20 18:51:35 +0000

    ...

--- collects/slatex/slatex-launcher.scm
+++ collects/slatex/slatex-launcher.scm
@@ -1,22 +1,14 @@
 (require-library "slatex.ss" "slatex")
-(define slatex::*texinputs* #f)
-(define slatex::*texinputs-list* #f)
-(let ([slatex-process-file
-       (lambda (fn)
-	 (let-values ([(base name dir?)
-		       (split-path fn)])
-	   (when (string? base) (current-directory base))
-	   (slatex::process-main-tex-file fn)))])
-  (case (system-type)
-    [(macos)
 
-     ;; set up drag and drop
-     (current-load slatex-process-file)
+(case (system-type)
+  [(macos)
 
-     (for-each slatex-process-file (vector->list argv))]
-    [(windows unix)
-     (when (eq? (vector) argv)
-       (error 'slatex "expected a file on the command line~n"))
-     (slatex-process-file (vector-ref argv 0))
-     (system (format "latex ~a" (vector-ref argv 0)))
-     (exit)]))
+   ;; set up drag and drop
+   (current-load slatex)
+
+   (for-each slatex (vector->list argv))]
+  [(windows unix)
+   (when (eq? (vector) argv)
+     (error 'slatex "expected a file on the command line~n"))
+   (slatex (vector-ref argv 0))
+   (exit)])
--- collects/slatex/slatex.ss
+++ collects/slatex/slatex.ss
@@ -1,1 +1,25 @@
-(require-library "slatexsrc.scm" "slatex" "slatex-code")
+(require-library "file.ss")
+
+(define slatex
+  (let ([ns (make-namespace)])
+    (parameterize ([current-namespace ns])
+      (require-library "slatexsrc.scm" "slatex" "slatex-code")
+      (global-defined-value 'slatex::*texinputs* #f)
+      (global-defined-value 'slatex::*texinputs-list* #f))
+    (lambda (file)
+      (unless (file-exists? file)
+	(error 'slatex "~e does not exist" file))
+      (let ([file (normalize-path file)])
+	(let-values ([(base name dir?) (split-path file)])
+	  (parameterize ([current-namespace ns]
+			 [current-directory
+			  (if (string? base)
+			      base
+			      (current-directory))])
+	    (eval `(slatex::process-main-tex-file ,name))))
+	(case (system-type)
+	  [(macos)
+	   (system "OTEX")
+	   (send-event "OTEX" "aevt" "odoc" (vector 'file file))]
+	   [(windows unix)
+	    (system (format "latex ~a" (vector-ref argv 0)))])))))

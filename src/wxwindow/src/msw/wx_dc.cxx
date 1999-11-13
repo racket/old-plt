af2f16ef10b96b0ce0805d81f3241f67589503a1
Matthew Flatt <mflatt@racket-lang.org>
1999-11-13 03:42:24 +0000

    .

--- src/wxwindow/src/msw/wx_dc.cxx
+++ src/wxwindow/src/msw/wx_dc.cxx
@@ -4,7 +4,7 @@
  * Author:	Julian Smart
  * Created:	1993
  * Updated:	August 1994
- * RCS_ID:      $Id: wx_dc.cxx,v 1.23 1999/10/26 21:32:04 mflatt Exp $
+ * RCS_ID:      $Id: wx_dc.cxx,v 1.24 1999/11/13 03:36:48 mflatt Exp $
  * Copyright:	(c) 1993, AIAI, University of Edinburgh
  */
 
@@ -34,7 +34,7 @@
 
 #endif
 
-#include "wx_rgn.h"
+#include "../../../wxcommon/Region.h"
 #include "wx_pdf.h"
 
 #include <math.h>

981c2c9afbfb665e4d57e90552d1f166846df12d
John Clements <clements@racket-lang.org>
2001-06-27 03:52:19 +0000

    ...

--- src/mac/mred/MrEdSetup.h
+++ src/mac/mred/MrEdSetup.h
@@ -9,16 +9,9 @@
 #endif
 #endif
 
-#define WX_STANDARD_GRAPHICS 1
-
-#define WX_FORCE_APP_CREATION 1
-#define WXS_CANT_ASSIGN_STRUCTURES 1
-
 #define WXUNUSED(x)
 
 #define OPERATOR_NEW_ARRAY
-
-#define WXME_FOR_MRED 1
 
 /* code added by JBC because compiler no longer handles keyword 
    'far' for PPC */

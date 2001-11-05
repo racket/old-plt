a8edaa995dff4d748673bead1337790902451e84
Matthew Flatt <mflatt@racket-lang.org>
2001-11-05 12:59:25 +0000

    .

--- src/wxxt/src/Utilities/String.cc
+++ src/wxxt/src/Utilities/String.cc
@@ -50,7 +50,7 @@ char *copystring_to_aligned(const char *s, int d)
   char *news;
 
   len = strlen(s + d);
-  news = new WXGC_ATOMIC char[len];
+  news = new WXGC_ATOMIC char[len + 1];
   memcpy(news, s + d, len + 1);
   return news;
 }

0ec44199209c9272425c01d6f1eb3005c9578550
Matthew Flatt <mflatt@racket-lang.org>
2002-10-27 19:37:01 +0000

    .

--- src/wxwindow/src/msw/wx_pdf.cxx
+++ src/wxwindow/src/msw/wx_pdf.cxx
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 1997-98 PLt (Matthew Flatt)
+/* Copyright (c) 1997-02 PLT (Matthew Flatt)
 
   This file exists because of a problem in Windows: when
   a built-in dialog is used (such as the FindFile dialog),
@@ -27,8 +27,6 @@ typedef struct {
   BOOL done;
   BOOL usedir;
   HWND hwnd;
-  HANDLE ready_sema;
-  HANDLE go_sema;
 } PrimData;
 
 static long DoPrim(void *data)
@@ -36,13 +34,6 @@ static long DoPrim(void *data)
   PrimData *_data = (PrimData *)data;
   MSG msg;
   long old;
-
-  // Create a queue - yuck!
-  // We have to do this so the queue can be joined to the
-  // main thread.
-  PeekMessage(&msg, NULL, NULL, NULL, PM_NOREMOVE);
-  ReleaseSemaphore(_data->ready_sema, 1, &old);
-  WaitForSingleObject(_data->go_sema, INFINITE);
 
   _data->result = _data->f(_data->data, _data->hwnd);
 
@@ -61,86 +52,82 @@ extern wxWindow *wxHWNDtoWindow(HWND);
 
 BOOL wxPrimitiveDialog(wxPDF f, void *data, int strict)
 {
-   long old;
-   DWORD id;
-   HANDLE th;
-   PrimData *_data = new PrimData;
-   BOOL result;
-   HWND top;
-   wxWindow *w;
+  long old;
+  DWORD id;
+  HANDLE th;
+  PrimData *_data = new PrimData;
+  BOOL result;
+  HWND top;
+  wxWindow *w;
 
-   _data->f = f;
-   _data->data = data;
-   _data->usedir = strict;
-   _data->done = 0;
-   _data->ready_sema = CreateSemaphore(NULL, 0, 1, NULL);
-   _data->go_sema = CreateSemaphore(NULL, 0, 1, NULL);
+  _data->f = f;
+  _data->data = data;
+  _data->usedir = strict;
+  _data->done = 0;
 
-   /* Make window child of frontmost if it exists in the
-      same context, and disable all others in the context. */
-   top = GetForegroundWindow();
-   w = wxHWNDtoWindow(top);
-   if (w) {
-     void *wc, *tc;
-     if (wxSubType(w->__type, wxTYPE_FRAME))
-       wc = ((wxFrame *)w)->context;
-     else if (wxSubType(w->__type, wxTYPE_DIALOG_BOX))
-       wc = ((wxDialogBox *)w)->context;
-     else
-       wc = NULL;
-     tc = wxGetContextForFrame();
-     if (wc != tc)
-       top = NULL;
-   } else
-     top = NULL;
+  /* Make window child of frontmost if it exists in the
+     same context, and disable all others in the context. */
+  top = GetForegroundWindow();
+  w = wxHWNDtoWindow(top);
+  if (w) {
+    void *wc, *tc;
+    if (wxSubType(w->__type, wxTYPE_FRAME))
+      wc = ((wxFrame *)w)->context;
+    else if (wxSubType(w->__type, wxTYPE_DIALOG_BOX))
+      wc = ((wxDialogBox *)w)->context;
+    else
+      wc = NULL;
+    tc = wxGetContextForFrame();
+    if (wc != tc)
+      top = NULL;
+  } else
+    top = NULL;
 
-   _data->hwnd = top;
+  _data->hwnd = top;
 
-   // Disable other windows:
-   wxList *disabled_windows;
-   wxChildNode *cnode;   
-   wxNode *node;   
-   disabled_windows = new wxList;
-   for (cnode = wxTopLevelWindows(NULL)->First(); cnode; cnode = cnode->Next()) {
-     wxWindow *w = (wxWindow *)cnode->Data();
-     if (w && cnode->IsShown()) {
-       if (w->GetHWND() != top) {
-	 disabled_windows->Append(w);
-	 w->InternalEnable(FALSE);
-       }
-     }
-   }
+  // Disable other windows:
+  wxList *disabled_windows;
+  wxChildNode *cnode;   
+  wxNode *node;   
+  disabled_windows = new wxList;
+  for (cnode = wxTopLevelWindows(NULL)->First(); cnode; cnode = cnode->Next()) {
+    wxWindow *w = (wxWindow *)cnode->Data();
+    if (w && cnode->IsShown()) {
+      if (w->GetHWND() != top) {
+	disabled_windows->Append(w);
+	w->InternalEnable(FALSE);
+      }
+    }
+  }
    
-   if (!(th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DoPrim, _data, 0, &id))) {
-     result = f(data, top);
-   } else {
-     struct Scheme_Thread_Memory *thread_memory = scheme_remember_thread((void *)th);
+  if (!(th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DoPrim, _data, 0, &id))) {
+    /* Thread creation failed! We're in trouble, but do `f' anywasy. */
+    result = f(data, top);
+  } else {
+    struct Scheme_Thread_Memory *thread_memory;
 
-     WaitForSingleObject(_data->ready_sema, INFINITE);
-     AttachThreadInput(id, GetCurrentThreadId(), TRUE);
-     ReleaseSemaphore(_data->go_sema, 1, &old);
+    thread_memory = scheme_remember_thread((void *)th);
 
-     wxDispatchEventsUntil(Check, (void *)_data);
+    /* There used to be a call to AttachThreadInput() here.
+       That was disasterous, because it interefered with the
+       carefulyl tuned event dispatching of mredmsw.cxx. */
 
-     WaitForSingleObject(th, INFINITE);
-     scheme_forget_thread(thread_memory);
-     CloseHandle(th);
+    wxDispatchEventsUntil(Check, (void *)_data);
 
-     result = _data->result;
-   }
+    WaitForSingleObject(th, INFINITE);
+    scheme_forget_thread(thread_memory);
+    CloseHandle(th);
 
-   // Restore other windows:
-   for (node = disabled_windows->First(); node; node = node->Next()) {
-     wxWindow *w = (wxWindow *)node->Data();
-     w->InternalEnable(TRUE);
-   }
+    result = _data->result;
+  }
 
-   delete disabled_windows;
+  /* Restore other windows: */
+  for (node = disabled_windows->First(); node; node = node->Next()) {
+    wxWindow *w = (wxWindow *)node->Data();
+    w->InternalEnable(TRUE);
+  }
 
-  if (_data->ready_sema)
-    CloseHandle(_data->ready_sema);
-  if (_data->go_sema)
-    CloseHandle(_data->go_sema);
+  delete disabled_windows;
 
   return result;
 }

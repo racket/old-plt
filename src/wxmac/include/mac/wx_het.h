3b013fe7934ffb9893dfd340ce83641aa155f3b1
Matthew Flatt <mflatt@racket-lang.org>
2003-02-26 23:24:39 +0000

    .

---
+++ src/wxmac/include/mac/wx_het.h
@@ -0,0 +1,8 @@
+
+#ifndef WX_HET_H
+#define WX_HET_H
+
+extern ControlPartCode wxHETTrackControl(ControlRef theControl, Point startPoint, ControlActionUPP actionProc);
+extern int wxHETYield(void);
+
+#endif

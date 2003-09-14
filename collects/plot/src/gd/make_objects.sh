2ecb1148b7b2ebbca2e5e8f1550298f4196db1f3
cozmic <>
2003-09-14 20:40:13 +0000

    *** empty log message ***

--- collects/plot/src/gd/make_objects.sh
+++
@@ -1,3 +0,0 @@
-cl /c /I../include  /DHAVE_LIBPNG *.c
-
-#link -lib  /nologo /out:libgd.lib gd.obj gd_gd.obj gd_gd2.obj gd_io.obj gd_io_dp.obj                 gd_io_file.obj gd_ss.obj gd_io_ss.obj gd_png.obj gd_jpeg.obj gdxpm.obj                 gdfontt.obj gdfonts.obj gdfontmb.obj gdfontl.obj gdfontg.obj                 gdtables.obj  gdcache.obj gdkanji.obj wbmp.obj                 gdhelpers.obj gd_topal.obj ../libpng/libpng.lib

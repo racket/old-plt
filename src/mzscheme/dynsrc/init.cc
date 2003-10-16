37cbfc7af5e8f6736ecc38ac1e314c59d79f00d7
Matthew Flatt <mflatt@racket-lang.org>
2003-10-16 00:33:19 +0000

    .

--- src/mzscheme/dynsrc/init.cc
+++ src/mzscheme/dynsrc/init.cc
@@ -19,13 +19,7 @@ extern "C" {
   int WINAPI dll_entry (HANDLE h, DWORD reason, void *ptr);
 };
 
-/* Magic that's needed if we use the Cygwin API */
-/* This is a global variable. */
-struct _reent *_impure_ptr;
-extern struct _reent *__imp_reent_data;
-
 int WINAPI dll_entry (HANDLE, DWORD reason, void *){
-  _impure_ptr = __imp_reent_data; /* last bit of magic */
   switch (reason) {
   case DLL_PROCESS_ATTACH:
     break;

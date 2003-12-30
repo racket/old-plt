d23061d5b95a26d286103a2135f5d4641b48fad0
Matthew Flatt <mflatt@racket-lang.org>
2003-12-30 16:13:46 +0000

    .

--- src/wxwindow/src/msw/wx_dc.cxx
+++ src/wxwindow/src/msw/wx_dc.cxx
@@ -1363,18 +1363,16 @@ void wxDC::SetMapMode(int mode)
   if (::GetMapMode(dc) != MM_ANISOTROPIC)
     ::SetMapMode(dc, MM_ANISOTROPIC);
 
-  if ((logical_scale_x != logical_scale_y)
-      && (::SetGraphicsMode(dc, GM_ADVANCED) == 0)) {
+  if (::SetGraphicsMode(dc, GM_ADVANCED) != 0) {
     XFORM xform;
-    xform.eM11 = logical_scale_x;
+    xform.eM11 = user_scale_x;
     xform.eM21 = 0;
-    xform.eM12 = logical_scale_y;
-    xform.eM22 = 0;
-    xform.eDx = logical_origin_x;
-    xform.eDy = logical_origin_y;
+    xform.eM12 = 0;
+    xform.eM22 = user_scale_y;
+    xform.eDx = device_origin_x;
+    xform.eDy = device_origin_y;
     ::SetWorldTransform(dc, &xform);
   } else {
-    ::SetGraphicsMode(dc, GM_COMPATIBLE);
     ::SetViewportExtEx(dc, 1000, 1000, NULL);
     ::SetWindowExtEx(dc, MS_XDEV2LOGREL(1000), MS_YDEV2LOGREL(1000), NULL);
     ::SetViewportOrgEx(dc, (int)device_origin_x, (int)device_origin_y, NULL);

1cfec72cede6aa8a5bf2d2c96947e350c37eb898
Eli Barzilay <eli@barzilay.org>
2004-10-25 06:35:20 +0000

    .

--- src/foreign/foreign.c
+++ src/foreign/foreign.c
@@ -13,15 +13,17 @@
 # include <inttypes.h>
 #else
 # include <windows.h>
-# include <wtypes.h>
-  typedef          _int8   int8_t;
-  typedef unsigned _int8  uint8_t;
-  typedef          _int16  int16_t;
-  typedef unsigned _int16 uint16_t;
-  typedef          _int32  int32_t;
-  typedef unsigned _int32 uint32_t;
-  typedef          _int64  int64_t;
-  typedef unsigned _int64 uint64_t;
+# ifndef __CYGWIN32__
+#  include <wtypes.h>
+   typedef          _int8   int8_t;
+   typedef unsigned _int8  uint8_t;
+   typedef          _int16  int16_t;
+   typedef unsigned _int16 uint16_t;
+   typedef          _int32  int32_t;
+   typedef unsigned _int32 uint32_t;
+   typedef          _int64  int64_t;
+   typedef unsigned _int64 uint64_t;
+# endif
 #endif
 #include "ffi.h"
 
